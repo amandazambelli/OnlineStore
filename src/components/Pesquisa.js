9f9068fdd1d1a43a2d4baf3c792bb4a2436d68ff
fkrein1 <felipekrein@gmail.com>
2022-04-26 17:03:31 -0300

    commit inicial

--- src/components/Pesquisa.js
+++ src/components/Pesquisa.js
@@ -4,7 +4,10 @@ class Pesquisa extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" />
+        <input type="text" data-testid="query-input" />
+        <button type="submit" data-testid="query-button" id="xpto">
+          Submit
+        </button>
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>

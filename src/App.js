b97ab9c433701af5f1a1eef379cc97edf9dbedbd
Amanda Zambelli <amandazambelli@ymail.com>
2022-04-26 17:05:09 -0300

    requisito 03

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, BrowserRouter } from 'react-router-dom';
 import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 import Pesquisa from './components/Pesquisa';
+import Carrinho from './components/Carrinho';
 
 class App extends React.Component {
   async componentDidMount() {
@@ -16,6 +17,7 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Route exact path="/" component={ Pesquisa } />
+        <Route exact path="/carrinho" component={ Carrinho } />
       </BrowserRouter>
     );
   }

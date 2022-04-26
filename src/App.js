86734d4f5e7486a09a4ee8f1efbae6b0489320b8
fkrein1 <felipekrein@gmail.com>
2022-04-26 18:18:51 -0300

    corrige lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Route, BrowserRouter } from 'react-router-dom';
-import Pesquisa from './components/Pesquisa';
 import Carrinho from './components/Carrinho';
+import Pesquisa from './components/Pesquisa';
 import BarraCategoria from './components/BarraCategoria';
 
 class App extends React.Component {
